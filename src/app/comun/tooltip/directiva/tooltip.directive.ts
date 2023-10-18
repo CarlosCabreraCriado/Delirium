3856b8b60398d7f24f79ca523a89ea1e7ce932d7
Carlos Cabrera <carloscabre_96@hotmail.com>
2023-10-18 11:58:04 +0200

    Creación Directiva tooltip

---
+++ src/app/comun/tooltip/directiva/tooltip.directive.ts
@@ -0,0 +1,14 @@
+
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: "[tooltip]",
+})
+
+export class TooltipDirective {}
+
+
+
+
+
+
---
+++ src/app/comun/tooltip/objetos/objeto.component.ts
@@ -0,0 +1,20 @@
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tooltipObjeto',
+  templateUrl: './objeto.component.html',
+  styleUrls: ['./objeto.component.sass']
+})
+
+export class TooltipObjetoComponent {
+
+  constructor() {}
+
+}
+
+
+
+
+
+
